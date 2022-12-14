aa0cfbaf3b1b1d0d88fc480346941562c822ad75
ahmerk-me <ahmer-k@outlook.com>
2022-12-14 18:16:09 +0000

    rollback

--- routes/users.js
+++ routes/users.js
@@ -60,36 +60,42 @@ module.exports = function (app, passport) {
         res.render('sign-in', contextDict);
     });
 
-    app.post('/sign-in', function(req, res, next) {
+    app.post('/sign-in', passport.authenticate('sign-in', {
+            successRedirect: '/usr/', // redirect to the secure profile section
+            failureRedirect: '/sign-in', // redirect back to the signup page if there is an error
+            failureFlash: true // allow flash messages
+        }) 
+        // function(req, res, next) {
 
-        var username = req.body.username
-        console.log("ahmer 444444444444 =======================================", username.length)
+        // var username = req.body.username
+        // console.log("ahmer 444444444444 =======================================", username.length)
 
-        if(username.length == 0) {
-            console.log("ahmer 33333333 ==================")
-            var checkOutNoti = 'Please fill all fields';
-            var contextDict = {
-                title: 'Sign In',
-                signInError: req.flash('signInError'),
-                checkOutNoti: checkOutNoti
-            };
+        // if(username.length == 0) {
+        //     console.log("ahmer 33333333 ==================")
+        //     var checkOutNoti = 'Please fill all fields';
+        //     var contextDict = {
+        //         title: 'Sign In',
+        //         signInError: req.flash('signInError'),
+        //         checkOutNoti: checkOutNoti
+        //     };
     
-            res.render('sign-in', contextDict);
+        //     res.render('sign-in', contextDict);
 
-        } else {
+        // } else {
 
-            passport.authenticate('sign-in', function(err, user, info) {
-                if (err) { 
+        //     if()
+            // passport.authenticate('sign-in', function(err, user, info) {
+            //     if (err) { 
 
-                    return res.render("/sign-in", err); }
-                // if (!user) { 
-                //     res.status(401);
-                //     res.end(info.message);
-                //     return;
-                // }
-                // createSendToken(req.user, res);
-                return res.redirect("/usr/" + req.body.username);
-              })(req, res, next);
+            //         return res.render("/sign-in", err); }
+            //     // if (!user) { 
+            //     //     res.status(401);
+            //     //     res.end(info.message);
+            //     //     return;
+            //     // }
+            //     // createSendToken(req.user, res);
+            //     return res.redirect("/usr/" + req.body.username);
+            //   })(req, res, next);
 
               
             // return passport.authenticate('sign-in', {
@@ -98,9 +104,9 @@ module.exports = function (app, passport) {
             //     failureFlash: true // allow flash messages
             // })
 
-        }
+        // }
 
-    }
+    // }
     );
 
     app.get('/sign-up', function (req, res) {
